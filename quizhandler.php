872bfbac78f933a447c30c9302e99c98198c097c
koolshibe <porgsgomeep@gmail.com>
2024-07-09 12:11:52 -0700

    Added backend validation

--- quizhandler.php
+++ quizhandler.php
@@ -5,9 +5,38 @@ include_once 'header.html';
 $personalities = ["pacifist", "fun guy", "normal", "extreme", "violent", "true ending"];
 $endings = ["liberator", "gamer", "colorblind"];
 
-$echostring = "";
-
 $bloodlust = 0;
+
+if (isset($_POST["name"])) {
+    echo "name: ". htmlspecialchars($_POST["name"]) . "<br>";
+} else {
+    echo "name is not set<br>";
+}
+if (isset($_POST["email"]) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    echo "email: ". htmlspecialchars($_POST["email"]). "<br>";
+} else {
+    echo "email is not set<br>";
+}
+if (isset($_POST["age"]) ) {
+    echo "age: " . htmlspecialchars($_POST["age"]). "<br>";
+} else {
+    echo "age is not set<br>";
+}
+if (isset($_POST["ssn"]) &&  ctype_digit($_POST["ssn"])) {
+    echo "ssn: " . htmlspecialchars($_POST["ssn"]). "<br>";
+} else {
+    echo "ssn is not set<br>";
+}
+if (isset($_POST["pet"])) {
+    echo "pet: " . htmlspecialchars($_POST["pet"]). "<br>";
+} else {
+    echo "pet is not set<br>";
+}
+if (isset($_POST["favorite"])) {
+    echo "favorite: " . htmlspecialchars($_POST["favorite"]). "<br>";
+} else {
+    echo "favorite is not set<br>";
+}
 
 for ($i = 1; $i<4; $i++) {
     $bloodlust += $_POST["smash_".$i]=="y" ? 1 : 0;
